ab66e435b57c9fb34a95ae20ba01816297d24574
AmritpaL-Singh0945 <amritpalsinghs0945@gmail.com>
2026-07-31 18:11:40 +0530

    Solution for is power of 2 in c

---
+++ Linux/Bitwise/isPower2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int isPower2(int x) {
+  return !(x & (x + ~0)) & !(x >> 31) & !!x;
+}
+
+int main() {
+  printf("isPower2(5) = %d\n", isPower2(5));
+  printf("isPower2(8) = %d\n", isPower2(8));
+  printf("isPower2(0) = %d\n", isPower2(0));
+  
+  return 0;
+}
