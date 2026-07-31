cf92abb09dde770eeb4dac66920525ef1545ac78
AmritpaL-Singh0945 <amritpalsinghs0945@gmail.com>
2026-07-31 17:43:04 +0530

    Solution for Form Fields

---
+++ Web_Programming/Form_Fields/script.js
@@ -0,0 +1,15 @@
+const fields = [
+    "First Name",
+    "Last Name",
+    "Email"
+];
+
+function generateForm() {
+    return fields.map(field => `
+        <label>${field}</label>
+        <input type="text">
+    `).join("");
+}
+
+document.getElementById("form-container").innerHTML = generateForm();
+
