b77eb3cb98f5c3233fe07f514b8af473cad588b5
AmritpaL-Singh0945 <amritpalsinghs0945@gmail.com>
2026-09-19 10:35:13 +0530

    Formatting of code of reverse Linked List

--- Java/LinkedList/ReverseLinkedList.java
+++ Java/LinkedList/ReverseLinkedList.java
@@ -1,50 +1,46 @@
-class Node{
-	int val;
-	Node next;
+class Node {
+  int val;
+  Node next;
 
-	Node(int val){
-		this.val=val;
-		this.next=null;
-	}
+  Node(int val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
+public class ReverseLinkedList {
+  public static Node reverseList(Node head, Node prev) {
+    if (head == null)
+      return prev;
+    Node Next = head.next;
+    head.next = prev;
+    return reverseList(Next, head);
+  }
 
-public class ReverseLinkedList{
+  public static void printList(Node head) {
+    if (head == null)
+      return;
 
-	public static Node reverseList(Node head,Node prev){
-		if (head==null) return prev;
-		Node Next=head.next;
-		head.next=prev;
-		return reverseList(Next,head);
-	}
+    Node temp = head;
+    while (temp != null) {
+      System.out.print(temp.val + "  ");
+      temp = temp.next;
+    }
+    System.out.println();
+  }
 
-	public static void printList(Node head){
-		if (head==null) return;
+  public static void main(String[] args) {
+    Node head = new Node(1);
+    Node t = head;
 
-		Node temp=head;
-		while (temp!=null){
-			System.out.print(temp.val +  "  ");
-			temp=temp.next;
-		}
-		System.out.println();
-	}
+    for (int i = 2; i <= 5; i++) {
+      Node temp = new Node(i);
+      t.next = temp;
+      t = t.next;
+    }
 
-	public static void main(String[] args){
-
-		Node head=new Node(1);
-		Node t=head;
-
-		for (int i=2;i<=5;i++){
-			Node temp=new Node(i);
-			t.next=temp;
-			t=t.next;
-		}
-
-		printList(head);
-		Node rev=reverseList(head,null);
-		printList(rev);
-
-
-	}
-	
+    printList(head);
+    Node rev = reverseList(head, null);
+    printList(rev);
+  }
 }
